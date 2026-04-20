7b5e24f0c5cd809481b0a6f955677685e65f8d42
Batyr Atayev <atayev2012@gmail.com>
2026-04-20 17:08:52 +0300

    Added basic user model

---
+++ backend/database/models.py
@@ -0,0 +1,20 @@
+from database import Base
+from sqlalchemy import Column, Integer, BigInteger, String, Date, ForeignKey, Boolean, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+# User model
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    
+    # Level 1: Student; Level 2: Employee; Level 3: Manager; Level 4: Admin
+    access_level: Mapped[int] = mapped_column(Integer, nullable=False)
+    
+    first_name: Mapped[str] = mapped_column(String(50), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=True)
+    middle_name: Mapped[str] = mapped_column(String(50), nullable=True)
+    user_img: Mapped[str] = mapped_column(String(255), nullable=True)
+ 
