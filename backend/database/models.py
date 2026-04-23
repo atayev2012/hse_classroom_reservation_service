693167cd41d5ed6bd43417d11d98d1021667af91
Batyr Atayev <atayev2012@gmail.com>
2026-04-23 21:00:05 +0300

    Added buildings, rooms and equipment models

--- backend/database/models.py
+++ backend/database/models.py
@@ -24,3 +24,45 @@ class UserVerification(Base):
 
     user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("users.id"), unique=True)
     verification_code: Mapped[str] = mapped_column(String(6), nullable=True)
+
+
+# University building
+class Building(Base):
+    __tablename__ = "buildings"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    address: Mapped[str] = mapped_column(String, nullable=False)
+
+    # relationships
+    rooms: Mapped[list["BuildingRoom"]] = relationship(back_populates="building")
+
+# Room in the building
+class BuildingRoom(Base):
+    __tablename__ = "building_rooms"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    capacity: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    type: Mapped[str] = mapped_column(String, nullable=False, default="classroom")
+    floor: Mapped[int] = mapped_column(Integer, nullable=True)
+
+    building_id: Mapped[int] = mapped_column(ForeignKey("buildings.id"))
+
+    # relationships
+    building: Mapped["Building"] = relationship(back_populates="rooms")
+    equipment: Mapped[list["BuildingRoomEquipment"]] = relationship(back_populates="room")
+    
+# Equipment that room conrains
+class BuildingRoomEquipment(Base):
+    __tablename__ = "building_room_equipment"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    qty: Mapped[int] = mapped_column(Integer, nullable=False, default=1)
+
+    room_id: Mapped[int] = mapped_column(ForeignKey("building_rooms.id"))
+
+    # relationships
+    room: Mapped["BuildingRoom"] = relationship(back_populates="equipment")
+
