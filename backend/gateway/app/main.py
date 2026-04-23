bab76ed65e6168725cf82566703991edc1fc86d8
Batyr Atayev <atayev2012@gmail.com>
2026-04-23 18:20:10 +0300

    Included reservation router

--- backend/gateway/app/main.py
+++ backend/gateway/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.auth_router.router import router as auth_router
+from app.reservation_router.router import router as room_reserv_router
 
 # Creating a FastAPI app
 app = FastAPI(
@@ -8,6 +9,8 @@ app = FastAPI(
     summary="REST API of a Classroom booking service for HSE university"
     )
 
+v1_web_api_prefix = "/api/v1/web"
 
 # Connecting routers
-app.include_router(auth_router, prefix="/api/v1/web")
+app.include_router(auth_router, prefix=v1_web_api_prefix)
+app.include_router(room_reserv_router, prefix=v1_web_api_prefix)
